620d396ddd2432c3abac7faecf89e3aacae769f4
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-13 16:33:30 +0700

    Information Function Complete

---
+++ inform-function.js
@@ -0,0 +1,5 @@
+const info = (name, location, hobby) => {
+  return `Hi, my name is ${name}. I live in ${location}, and enjoy ${hobby}`;
+};
+
+console.log(info("Chang", "Bangkok", "reading books."));
