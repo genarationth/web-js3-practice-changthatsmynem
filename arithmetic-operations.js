9a537086a7fef7ab5abf9c166339454c2d9632dd
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-13 16:22:41 +0700

    Arithmetic Practice with Functions

---
+++ arithmetic-operations.js
@@ -0,0 +1,27 @@
+//Addition Operation - declaration function
+function addOperands(a, b) {
+  return a + b;
+}
+
+console.log(addOperands(3, 9));
+
+//Subtraction Operation - expression function
+const subtract = function (a, b) {
+  return a - b;
+};
+
+console.log(subtract(28, 1));
+
+//Multiplication Operation - arrow function
+const multiply = (a, b) => {
+  return a * b;
+};
+
+console.log(multiply(125, 225));
+
+//Division Operation - arrow function
+const divide = (a, b) => {
+  return a / b;
+};
+
+console.log(divide(236, 46));
